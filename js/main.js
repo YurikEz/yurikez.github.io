10f4823e95e87ef3939ca1983ca6c71d1584ef4e
Yurik "YurikEz" Elmanov <elmanovyurik@gmail.com>
2018-10-30 02:56:17 +0300

    update css/js/html
    create modal image html
    create modal image css
    create modal image js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 window.onload = function() {	
 
-	// START smooth link
+// START smooth link
 	// собираем все якоря; устанавливаем время анимации и количество кадров
 	const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
 	animationTime = 300,
@@ -42,61 +42,119 @@ window.onload = function() {
 // END smooth link
 
 // START Validate form
-	const fos = [].slice.call(document.getElementById("fos"));
+	let message = document.getElementById("message");
+	let email = document.getElementById("email");
+	let send = document.getElementById("send");
 
-	fos.forEach(function(item) {
-		let message = document.getElementById("message");
-		let email = document.getElementById("email");
-		let send = document.getElementById("send");
-
-		let message_error = document.getElementById("message_error");
-		let email_error = document.getElementById("email_error");
+	let message_error = document.getElementById("message_error");
+	let email_error = document.getElementById("email_error");
 
 
-		let reg_email = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-		let reg_message = /^[а-яА-ЯёЁa-zA-Z0-9_\-\.\!\,\?]+$/;
+	let reg_email = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+	let reg_message = /^[а-яА-ЯёЁa-zA-Z0-9_\-\.\!\,\?]+$/;
 
-		if (item == message) {
-			message.oninput = function() {
-				if (message.value.length == 0) {
-					message.style.border = "";
-					message_error.style.display = "";
-				} else if (reg_message.test(message.value) == false) {
-					message.style.border = "2px solid red";
-					message_error.style.display = "block";
-				} else {
-					message.style.border = "2px solid #6DB65B";
-					message_error.style.display = "";
-				}	
-			};
+	// обработчик ввода сообщения в форму id=message
+	message.oninput = function() {
+		if (message.value.length == 0) {
+			message.style.border = "";
+			message_error.style.display = "";
+		} else if (reg_message.test(message.value) == false) {
+			message.style.border = "2px solid red";
+			message_error.style.display = "block";
+		} else {
+			message.style.border = "2px solid #6DB65B";
+			message_error.style.display = "";
+		}	
+	};
+	// обработчик ввода email в форму id=email
+	email.oninput = function() {
+		if (email.value.length == 0) {
+			email.style.border = "";
+			email_error.style.display = "";
+		} else if (reg_email.test(email.value) == false) {
+			email.style.border = "2px solid red";
+			email_error.style.display = "block";
+		} else {
+			email.style.border = "2px solid #6DB65B";
+			email_error.style.display = "";
 		}
-		if (item == email) {
-			email.oninput = function() {
-				if (email.value.length == 0) {
-					email.style.border = "";
-					email_error.style.display = "";
-				} else if (reg_email.test(email.value) == false) {
-					email.style.border = "2px solid red";
-					email_error.style.display = "block";
-				} else {
-					email.style.border = "2px solid #6DB65B";
-					email_error.style.display = "";
-				}
-			};
-		}
+	};
+	// обработчик нажатия кнопки отправки
+	send.addEventListener('click', function(e) {
+		// убираем стандартное поведение
+		e.preventDefault();
 
-		if (item == send) {
-			send.addEventListener('click', function(e) {
-				// убираем стандартное поведение
-				e.preventDefault();
-
-				if (reg_email.test(email.value) == true && reg_message.test(message.value) == true) {
-					alert('Сообщение отправлено!');
-				} else {
-					alert('Заполните все поля');
-				}
-			});
+		if (reg_email.test(email.value) == true && reg_message.test(message.value) == true) {
+			alert('Сообщение отправлено!');
+		} else {
+			alert('Заполните все поля');
 		}
 	});
 // END Validate form
+
+// START Modal Image
+
+	const image = [].slice.call(document.querySelectorAll('img[src*="img/work/min/"]'));
+
+	let modal_image = document.getElementById("modal_image");
+	let modal = document.getElementById("modal");
+
+	let prev = document.getElementById("prev");
+	let next = document.getElementById("next");
+	let close = document.getElementById("close");
+
+	image.forEach(function(item) {
+		// каждому якорю присваиваем обработчик события
+		item.addEventListener('click', function(e) {
+			// убираем стандартное поведение
+			e.preventDefault();
+
+			let imageSrc = item.getAttribute('src');
+			let imageAlt = item.getAttribute('alt');
+
+			modal_image.setAttribute("src", imageSrc.replace("min", "large"));
+			modal_image.setAttribute("alt", imageAlt);
+			modal.style.display = "grid";
+
+			// отслеживаем нажатие на кнопку id=prev, перелистываем слайд назад
+			prev.addEventListener('click', function(e) {
+				// убираем стандартное поведение
+				e.preventDefault();
+
+				let imageSrc = modal_image.getAttribute('src');
+				let imageAlt = modal_image.getAttribute('alt');				
+				if (imageSrc.charAt(20) - 1 > 0) {
+					modal_image.setAttribute("src", imageSrc.replace(imageSrc.charAt(20), imageSrc.charAt(20) - 1));
+					modal_image.setAttribute("alt", imageAlt.replace(imageAlt.charAt(5), imageAlt.charAt(5) - 1));
+				} else {
+					modal_image.setAttribute("src", imageSrc.replace(imageSrc.charAt(20), imageSrc.charAt(20) - 1 + image.length));
+					modal_image.setAttribute("alt", imageAlt.replace(imageAlt.charAt(5), imageAlt.charAt(5) - 1 + image.length));
+				}
+			});
+
+			// отслеживаем нажатие на кнопку id=next, перелистываем слайд вперед
+			next.addEventListener('click', function(e) {
+				// убираем стандартное поведение
+				e.preventDefault();
+
+				let imageSrc = modal_image.getAttribute('src');
+				let imageAlt = modal_image.getAttribute('alt');				
+				if (+imageSrc.charAt(20) + 1 <= image.length) {
+					modal_image.setAttribute("src", imageSrc.replace(imageSrc.charAt(20), +imageSrc.charAt(20) + 1));
+					modal_image.setAttribute("alt", imageAlt.replace(imageAlt.charAt(5), +imageAlt.charAt(5) + 1));
+				} else {
+					modal_image.setAttribute("src", imageSrc.replace(imageSrc.charAt(20), imageSrc.charAt(20) - image.length + 1));
+					modal_image.setAttribute("alt", imageAlt.replace(imageAlt.charAt(5), imageAlt.charAt(5) - image.length + 1));
+				}		
+			});
+			// отслеживаем нажатие на кнопку id=close, закрываем модальное окно
+			close.addEventListener('click', function(e) {
+				// убираем стандартное поведение
+				e.preventDefault();
+
+				modal.style.display = "";
+			});
+		});
+	});
+// END Modal Image
 }
